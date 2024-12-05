34df604be1afaf17ae67c27f17e38c61640a1d29
Bill Crossley <efpophis@efpophis.net>
2024-12-05 11:43:13 -0500

    stop before disable

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo systemctl stop net-efpophis-spots
 sudo systemctl disable net-efpophis-spots
 sudo rm -f /etc/systemd/system/net-efpophis-spots.service
 sudo rm -f /usr/local/scripts/spot_rpt
