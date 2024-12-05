638a9e0da62970b677e448aff3831db05da65f59
Bill Crossley <efpophis@efpophis.net>
2024-12-05 12:08:49 -0500

    refining install

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+read -p "What is your call sign? " user_call
+read -p "Which dxcluster host do you want to use? [dxusa.net] " cl_host
+read -p "   Which port? [7300] " cl_port
+
+if [ -z "$user_call" ]; then
+    echo "You must specify your callsign"
+    exit
+fi
+
+cl_host=${cl_host:-dxusa.net}
+cl_port=${cl_port:-7300}
+
 if [ -x build.sh ]; then
     ./build.sh
 fi
@@ -9,6 +21,9 @@ if [ -d dist ]; then
     mkdir -p /usr/local/scripts
     sudo install -m755 spot_rpt /usr/local/scripts
     sudo install net-efpophis-spots.service /etc/systemd/system
+    sudo sed -e "s/CLUSTER_HOST/$cl_host/g" /etc/systemd/system/net-efpophis-spots.service
+    sudo sed -e "s/CLUSTER_PORT/$cl_port/g" /etc/systemd/system/net-efpophis-spots.service
+    sudo sed -e "s/CALLSIGN/$user_call/g" /etc/systemd/system/net-efpophis-spots.service
 
     sudo systemctl enable net-efpophis-spots.service
     popd
